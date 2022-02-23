6babdae73cce24afc56b7ff23caff5f0b635d311
nishanbe <Msjammsjam1!>
2022-02-23 16:13:08 +1100

    Initial Commit

--- Week 3/w3_prac4.py
+++ Week 3/w3_prac4.py
@@ -5,10 +5,8 @@ x = 10
 x += 5  # this means x = x + 5
 print(x)
 
-score = 0
+score = 0  # Initial score
 score += 1  # Earned 1
 score += 1  # Earned 1
 score += 1  # Earned 1
-print("Final score:", score)
-
-
+print("Final score:", score)  # The score will be 3
