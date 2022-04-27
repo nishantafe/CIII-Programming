6f5c23cff72f83cea1cb7853b89226c8df9e57f1
Nishan <nishantafe@gmail.com>
2022-04-27 16:05:58 +1000

    Initial Commit

---
+++ Week 10/w10_prac1.py
@@ -0,0 +1,13 @@
+listOfThings = ["car", "boat", "caravan", "motor cycle", 42]
+
+# Example of slicing a list
+print(listOfThings[0:2])  # prints ['car', 'boat']
+
+# Example of slicing a string
+name = "birthday"
+print(name[0:5])  # this prints "birth"
+print(name[5:8])  # this prints "day"
+
+# You can perform a calculation on a list item (int)
+result = 5 + listOfThings[4]
+print(result)
---
+++ Week 10/w10_prac2.py
@@ -0,0 +1,45 @@
+import math
+import string
+import random
+import time
+
+my_pi = math.pi;
+print(my_pi)
+
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
+
+print(letters)
+print(digits)
+print(symbols)
+
+print("------------------------------------")
+
+print("This program will count the number of letters, digits and symbols in any give word")
+word = input("Enter a word: ")
+letters_count = 0
+digits_count = 0
+symbols_count = 0
+
+for letter in word:
+    if letter in letters:
+        letters_count += 1
+    if letter in digits:
+        digits_count += 1
+    if letter in symbols:
+        symbols_count += 1
+print("Letters count = ", letters_count)
+print("Digits count = ", digits_count)
+print("Symbols count = ", symbols_count)
+
+#
+# if "!" in symbols:
+#     print("found")
+# else:
+#     print("not found")
+
+for i in range(20):
+    time.sleep(10)
+    random_number = random.randint(1, 10)
+    print("The random number is: ", random_number)
