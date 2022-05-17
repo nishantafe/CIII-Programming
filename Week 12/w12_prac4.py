162d7c9101acb6eed931861a8bf0678a1431022a
nishanbe <Msjammsjam1!>
2022-05-17 15:14:36 +1000

    Change the while loop condition

--- Week 12/w12_prac4.py
+++ Week 12/w12_prac4.py
@@ -1,10 +1,9 @@
 number = input("Input a number: ")
-
-while number != int:
+while True:  # Keep looping
     try:
-        number = int(number)
+        number = int(number)  # Try casting to an integer
         is_num = True
-        break
+        break  # Exit the loop
     except:
         number = input("Input a number: ")
 print(number, is_num)
